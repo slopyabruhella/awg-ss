2b2ce33146e96c7ff789197e15a27b7bab770d67
Slopya Bruhella <bruh@bruh.com>
2026-01-24 17:16:36 +0300

    bruh moment

--- init.sh
+++ init.sh
@@ -17,19 +17,38 @@ do
     COUNTER=$(( COUNTER + 1 ))
     basename=$(basename ${s})
     name=${basename%.conf}
+    echo "Docker subnet $dockersubnet via $dockergw"
     echo awg interface "${name}" will be created from config file "${basename}"
     cp ${s} /etc/amnezia/amneziawg/${name}.conf
     chmod 600 /etc/amnezia/amneziawg/${name}.conf
     resolvconf -u
     awg-quick up ${name} &
-    sleep 4
-    /usr/bin/ssserver -vc /config/config.json --outbound-bind-interface ${name} &
-    ss_pid=$!
+    sleep 3
     ###
+    # Restoring default local routes
+    ###
+    echo "Fixing local network routes after PostUp AWG"
+    DROUTE=$(ip route | grep default | awk '{print $3}');
+    HOMENET=192.168.0.0/16;
+    HOMENET2=10.0.0.0/8;
+    HOMENET3=172.16.0.0/12;
+    ip route add $HOMENET3 via $DROUTE;
+    ip route add $HOMENET2 via $DROUTE;
+    ip route add $HOMENET via $DROUTE;
+    iptables -I OUTPUT -d $HOMENET -j ACCEPT;
+    iptables -A OUTPUT -d $HOMENET2 -j ACCEPT;
+    iptables -A OUTPUT -d $HOMENET3 -j ACCEPT;
+    iptables -A OUTPUT ! -o ${name} -m mark ! --mark $(awg show ${name} fwmark) -m addrtype ! --dst-type LOCAL -j REJECT
+    ###
+    # Restored local routes
+    ###
+    /usr/bin/ssserver -vc /config/config.json &
+    ss_pid=$!
+    sleep 3
+    ### --outbound-bind-interface ${name}
     #iptables -A FORWARD -i ${name} -j ACCEPT
     #iptables -A FORWARD -o ${name} -j ACCEPT
     #iptables -A FORWARD -i ${name} -o ${name} -j ACCEPT
-    sleep 6
     while kill -s 0 $ss_pid && awg show | grep -q ${name}
     do
       echo "Current IP: $(wget -q -O - http://ipecho.net/plain)"
